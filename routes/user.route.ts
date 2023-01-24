e93b9fe2a3be64165640216d5c9c072d427bb881
Bunyasit Fang <wachaa1319@gmail.com>
2023-01-24 12:27:59 +0700

    login with email

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -19,6 +19,31 @@ usersRouter
     const user = await Users.insertOne(body);
     ctx.response.body = user;
   })
+  .post("/login/email", async (ctx) => {
+    const body = await ctx.request.body({ type: "json" }).value;
+
+    if (!body.email || !body.password) {
+      ctx.response.body = { error: "Email or password not provided" };
+      return;
+    }
+
+    const users = await Users.find({
+      email: body.email,
+    }).toArray();
+
+    console.log(users);
+
+    // check password
+    const user = users.find((user) => {
+      return user.password === body.password;
+    });
+
+    if (user) {
+      ctx.response.body = user;
+    } else {
+      ctx.response.body = { error: "User not found" };
+    }
+  })
   .put("/:id", async (ctx) => {
     const body = await ctx.request.body({ type: "json" }).value;
     const user = await Users.updateOne(
